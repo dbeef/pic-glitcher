d91d9e8043d979ce64efdfd72e335a0608a8365f
d.zalega <daniel.zalega@gmail.com>
2017-04-04 23:17:52 +0200

    Works

--- src/App.java
+++ src/App.java
@@ -19,20 +19,23 @@ public class App {
         String inputFileName = null;
         int numberOfCopies = 0;
         int numberOfBytes = 0;
+        boolean swapBytesAcrossAllSections = false;
 
         createDirectory();
 
         System.out.println("pic-glitcher " + VERSION);
-        if (args.length == 3) {
+        if (args.length == 4) {
             inputFileName = args[0];
             numberOfCopies = Integer.parseInt(args[1]);
             numberOfBytes = Integer.parseInt(args[2]);
+            swapBytesAcrossAllSections = Boolean.getBoolean(args[3]);
         } else {
             System.out.println("No arguments provided on start.");
-            System.out.println("Correct order to run pic-glitcher is java -jar picture.jpg 500 2");
+            System.out.println("Correct order to run pic-glitcher is java -jar picture.jpg 500 2 false");
             System.out.println("Where picture.jpg stands for the picture to glitch");
             System.out.println("500 is the number of new, randomly glitched images");
             System.out.println("2 is the number of bytes to swap per one image (suggested 2 or more)");
+            System.out.println("false is an argument for swapping bytes not only from image scan section");
             System.exit(0);
         }
 
@@ -43,12 +46,20 @@ public class App {
         is.read(buffer);
         is.close();
 
+        int beginIndex = 0;
         int endIndex = 0;
 
         //Wyszukuje ostatnie wystapienie
         for (int a = 0; a < buffer.length; a++) {
             if (Integer.toHexString(buffer[a]).contains(SCAN_END))
                 endIndex = a;
+        }
+        //Wyszukuje pierwsze wystapienie
+        for (int a = 0; a < buffer.length; a++) {
+            if (Integer.toHexString(buffer[a]).contains(SCAN_BEGINNING)) {
+                beginIndex = a;
+                break;
+            }
         }
 
         for (int b = 0; b < numberOfCopies; b++) {
@@ -58,7 +69,12 @@ public class App {
             os = new FileOutputStream(new File("pic-glitcher-pictures/copy-" + b + ".jpg"));
 
             for (int g = 0; g < numberOfBytes; g++) {
-                int whichToChange = ThreadLocalRandom.current().nextInt(0, endIndex);
+                int whichToChange = 0;
+                if (swapBytesAcrossAllSections)
+                    whichToChange = ThreadLocalRandom.current().nextInt(0, buffer.length);
+                else
+                    whichToChange = ThreadLocalRandom.current().nextInt(beginIndex, endIndex);
+
                 byte valueOfSwappedByte = (byte) ThreadLocalRandom.current().nextInt(-127, 128 + 1);
                 tempBuffer[whichToChange] = valueOfSwappedByte;
             }
